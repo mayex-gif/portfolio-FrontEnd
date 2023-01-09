4385c396eb3880fc4146dcd988b1cf54b905ee9f
mayex-gif <alcaldealejo53@gmail.com>
2023-01-09 16:35:26 -0300

    Correcciones FrontEnd

--- src/app/components/foto-perfil/foto-perfil.component.ts
+++ src/app/components/foto-perfil/foto-perfil.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-foto-perfil',
   templateUrl: './foto-perfil.component.html',
   styleUrls: ['./foto-perfil.component.css']
 })
-export class FotoPerfilComponent {
+export class FotoPerfilComponent implements OnInit {
+  persona: persona = new persona("", "", "");
+
+  constructor(public personaService: PersonaService) { }
+
+  ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data => { this.persona = data })
+  }
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {
+  }
 
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.router.navigate(['/login'])
+  }
 }
