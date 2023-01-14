a31b48debefd4bc137e1b48268b899d98dd3adf2
mayex-gif <alcaldealejo53@gmail.com>
2023-01-14 01:41:32 -0300

    Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
+import { NewSkillComponent } from './components/hard-and-soft-skills/new-skill.component';
+import { EditSkillComponent } from './components/hard-and-soft-skills/edit-skill.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'nuevaexperiencia', component: NewExperienciaComponent },
+  { path: 'editexp/:id', component: EditExperienciaComponent },
+  { path: 'nuevaeducacion', component: NewEducacionComponent },
+  { path: 'editeduc/:id', component: EditEducacionComponent },
+  { path: 'nuevaskill', component: NewSkillComponent },
+  { path: 'editskill/:id', component: EditSkillComponent },
 ];
 
 @NgModule({
